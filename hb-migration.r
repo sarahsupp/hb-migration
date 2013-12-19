e2a64f906022e5bb7d82c2fa69fcacfef4ba3b7f
sarahsupp <sarah@weecology.org>
2013-12-19 15:59:41 -0500

    adding a file to read and clean the eBird data for analysis

---
+++ hb-migration.r
@@ -0,0 +1,14 @@
+#Code for eBird migration project
+# (c) 2013 Sarah Supp 
+
+#set working directory
+wd = "C:/Users/sarah/Documents/eBird_data/"
+setwd(wd)
+
+# read in eBird data
+bchu = read.table('ebd_bkchum_200801_201312_relOct-2013/ebd_bkchum_200801_201312_relOct-2013.txt',
+                  header=TRUE, sep="\t", fill=TRUE, nrows=10)
+ruhu = read.table('ebd_rufhum_200801_201312_relOct-2013/ebd_rufhum_200801_201312_relOct-2013.txt', 
+                  header=TRUE, sep="\t", fill=TRUE, nrows=10)
+
+#
