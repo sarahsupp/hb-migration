b40ba8b4010c2a1b340388203356f3bcdbf5553a
sarahsupp <sarah@weecology.org>
2014-01-03 13:12:12 -0500

    started file to calculate centroids for daily occurrence based on La Sorte et al. 2013 migration paper code

---
+++ hb-centroids.r
@@ -0,0 +1,20 @@
+# Code for tracking hummingbird migration
+# Modified from code for La Sorte et al. 2013. (Population-level scaling of 
+# avian migration speed with body size and migration distance for powered fliers, Ecology)
+# Sarah R. Supp (c) 2014
+
+library(spaa) #failed to install on mac
+library(fields)
+library(colorRamps) #failed to install on mac
+library(maptools)
+library(fossil) #failed to install on mac
+library(PBSmodelling)
+library(aspace)
+library(pastecs)
+#library(raster)
+library(mgcv)
+library(adehabitat)
+library(SDMTools)
+library(xlsx)
+library(gamm4)
+
