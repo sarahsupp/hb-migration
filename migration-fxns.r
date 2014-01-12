7b4ecd6de481c5bb8c755e25f1345ef94c21c4a3
sarahsupp <sarah@weecology.org>
2014-01-12 12:59:04 -0500

    added code to use gam function to fit data in distance traveled

--- migration-fxns.r
+++ migration-fxns.r
@@ -88,6 +88,7 @@ MeanDailyLoc = function(yeardata, species) {
 DailyTravel = function(meanlocs){
   #use geodist to calculate Great Circle distance between daily location centers
   require(spaa)
+  require(mgcv)
   dst=c(NA)
   
   for(i in 1:nrow(meanlocs)){
@@ -104,11 +105,11 @@ DailyTravel = function(meanlocs){
   print (ggplot(distdat, aes(jday, dst)) + geom_line(size=1) + theme_bw() + xlab("Julian Day") + 
     ylab("Distance Traveled (km)") + ggtitle(distdat[1,1]))
   
-  #TODO: get a better fit line here
   #TODO: use La Sorte method to fit gamm and determine threshold for beginning 
   # of spring and end of fall migration for each species and year; Fig A4 in 2013 article
   print (ggplot(distdat, aes(jday, count)) + geom_point() + theme_bw() + xlab("Julian Day") + 
-           ylab("Number of observances") + ggtitle(distdat[1,1]) + stat_smooth(col="indianred"))
+           ylab("Number of observances") + ggtitle(distdat[1,1]) + 
+           geom_smooth(se=T, method='gam', formula=y~s(x), color='indianred'))
   
     return (dst)
 }
