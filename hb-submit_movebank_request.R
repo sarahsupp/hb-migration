c8589dace9179eb416a8ce0799bc3b4e0aef13b0
Tina Cormier <tcormier@whrc.org>
2014-04-23 18:09:31 -0400

    rewrote shell scripts in R so I can write to the psql database. Initial commit. Still a work in progress

---
+++ hb-submit_movebank_request.R
@@ -0,0 +1,83 @@
+# Purpose: This R script uses curl to submit an annotation request to movebank.
+# The arguments required include: url=url to which to submit request; xy=a csv file containing a list
+# of xy coordinates for annotation (see "/Users/tcormier/Documents/820_Hummingbirds/prelim_analyses/movebank/auto_submit/test_calliope.csv")
+# for an example of how it must be structured); xml=an xml file containing the request parameters (i.e. sensor, product, 
+# interpolation etc. See /Users/tcormier/Documents/820_Hummingbirds/prelim_analyses/movebank/auto_submit/NDVI_EVI.xml for
+# example); un=movebank username; pw=movebank password; outdir=output directory for returned xml document with access key.
+#
+# Author: Tina Cormier
+# Date: April 21, 2014
+# Status: In Progress
+
+
+
+library(RPostgreSQL)
+library(XML)
+
+
+options(echo=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
+
+url <- args[1]
+xy <- args[2]
+xml <- args[3]
+un <- args[4]
+pw <- args[5]
+outdir <- args[6]
+
+###############################
+# #hardcoded for testing:
+#
+#address to submit request
+url <- "http://www.bioinfo.mpg.de/orn-gateway/request-annotation-xml.jsp"
+
+#Text file of xy locations of points to be annotated. Must have the following columns
+#with no spaces: Timestamp, location-long, location-lat, height-above-ellipsoid (optional)
+xy <- "/Users/tcormier/Documents/820_Hummingbirds/prelim_analyses/movebank/auto_submit/test_calliope.csv"
+
+#XML containing movebank request details (see hb_movebank_createXML.py)
+xml <- "/Users/tcormier/Documents/820_Hummingbirds/prelim_analyses/movebank/auto_submit/NDVI_EVI.xml"
+
+#user name
+un <- "tcormier"
+
+#password
+pw <- "nohg3ITh"
+
+#outdir
+outdir <- "/Users/tcormier/Documents/820_Hummingbirds/prelim_analyses/movebank/auto_submit/submitted_requests/"
+outfile <- "/Users/tcormier/Documents/820_Hummingbirds/prelim_analyses/movebank/auto_submit/submitted_requests/testR.xml"
+##############################
+#connect to db
+# Connect to database
+con <- dbConnect(drv="PostgreSQL", port="5433",host="fusion", dbname="tcormier")
+#dbListTables(con)
+
+#Curl command will return an xml with the access key in it - filepath of that returned file:
+now <- format(Sys.time(), format="%Y%m%d_%H%M%S")
+ret.xml <- paste(outdir, "/", unlist(strsplit(basename(xy), "\\."))[1], "_", unlist(strsplit(basename(xml), "\\."))[1], "_", now, ".xml", sep="")
+
+#system call to curl (was able to get this to work somehow when I couldn't get Rcurl to work! - just need to plug ahead for now.)
+curl.cmd <- paste("curl -o ", ret.xml, " -F \"request=@", xml, "\" -F \"tracks=@", xy, "\" -F \"login=", un, "\" -F \"password=", pw, "\" ", url, sep="")
+system(curl.cmd)
+
+#check to see if ret.xml exists (it should if the curl system call did not produce an error).
+#If it exists, write accesskey to db
+if (file.exists(ret.xml)) {
+  #parse returned xml file
+  doc <- xmlTreeParse(ret.xml)
+  r <- xmlRoot(doc)
+  ak <- xmlAttrs(r)[names(xmlAttrs(r))=="accessKey"]
+  ak.status <- xmlAttrs(r)[names(xmlAttrs(r))=="status"]
+  
+  #FIX this to non-test table when script is stable and production-ready
+  akl.query <- paste("INSERT INTO test_access_key_list (access_key) VALUES (", ak, ");", sep="")
+  insert.ak <- dbGetQuery(con, akl.query)
+  
+  #talk to Jesse about making this a real date-time
+  status.query <- paste("INSERT INTO test_access_key_lut (access_key, date_time, tracks, xml, status) VALUES (", ak, ",", now, ",", xy, ",", xml, ",", ak.status, ");", sep="")
+  insert.ak <- dbGetQuery(con, status.query)
+}
+
+
+outfile <- 
