c1cab005ed7ba812e0127f2521201e696ee32f90
sarahsupp <sarah@weecology.org>
2014-04-25 10:16:18 -0400

    cleans up the hexgrid file too so only need to import the smaller window of global data for project

--- aggregate_hb_checklists.r
+++ aggregate_hb_checklists.r
@@ -1,3 +1,9 @@
+# Cleans up the data needed for the hummingbird migration project. 
+# Only needs to be done once with the raw files sent from FAL
+wd = "C:/Users/sarah/Dropbox/ActiveResearchProjects/Hummingbird_eBirdMigration/data"
+setwd(wd)
+
+
 # Aggregate the files by species or put them all together
 files = list.files(path = getwd(), pattern = "hex-12_checklists_*", recursive=TRUE, full.names=TRUE)
 
@@ -36,3 +42,19 @@ write.table(rthu, file = paste(writewd,"rthu.txt", sep=""), row.names=FALSE)
 write.table(cahu, file = paste(writewd,"cahu.txt", sep=""), row.names=FALSE)
 
 
+#clean up the icosahedron file so it doesn't have to be brought in globally each time
+hexgrid = readShapePoly(paste(getwd(), "/icosahedron_land_and_sea/cell_out2.shp", sep=""))
+hexpoints = readShapePoints(paste(getwd(), "/icosahedron_land_and_sea/point_out2.shp", sep=""))
+hexlonlat = data.frame(ID = hexpoints@data$global_id, LON = hexpoints@coords[,1], LAT = hexpoints@coords[,2])
+hexlonlat$POLYFID = as.integer(as.character(hexlonlat$ID))
+
+hexgrid$POLYFID = hexlonlat$POLYFID
+hexgrid$LATITUDE = hexlonlat$LAT
+hexgrid$LONGITUDE = hexlonlat$LON
+
+# crop to just North America, where the migratory species occur
+hexgrid = hexgrid[which(hexgrid$LATITUDE > 10 & hexgrid$LATITUDE <75 & 
+                          hexgrid$LONGITUDE > -178 & hexgrid$LONGITUDE < -50),]
+
+#write cropped file to the data folder
+writePolyShape(hexgrid, "icosahedron.shp")
