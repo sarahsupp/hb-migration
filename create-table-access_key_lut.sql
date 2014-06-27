62cbecaff08196341d6c52c63f82dd3cb3eb716a
Tina Cormier <tcormier@whrc.org>
2014-06-27 18:21:05 -0400

    added annotation file and spp fields

--- create-table-access_key_lut.sql
+++ create-table-access_key_lut.sql
@@ -1,8 +1,10 @@
 CREATE TABLE access_key_lut (
 	ak_id serial NOT NULL PRIMARY KEY,
-	access_key numeric NOT NULL,
+	access_key text NOT NULL,
 	date_time text,
 	tracks text,
 	xml text,
-	status text
+	status text,
+	ann_name text,
+	spp text
 	);
