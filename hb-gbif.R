2fbdfb2d577a57a91456b9def78955267e14cd0a
sarahsupp <sarah@weecology.org>
2014-07-29 16:26:51 -0400

    deleted lines of code from tutorial. Loops through species and countires to plot locations in Central America, aka winter range

--- hb-gbif.R
+++ hb-gbif.R
@@ -11,25 +11,34 @@ library(httr)
 library(maps)
 library(ggplot2)
 
+
 species = c("Archilochus colubris", "Archilochus alexandri", "Selasphorus platycercus", 
-           "Selasphorus calliope", "Selasphorus rufus")
+           "Stellula calliope", "Selasphorus rufus") #note: must use synonym for "Selasphorus calliope"
 centralamerica = c("MX", "GT", "SV", "HN", "CR", "PA", "BZ", "NI")
+centralamerica_names = c("Mexico","Honduras","Guatemala","El Salvador","Panama","Belize","Costa Rica","Nicaragua")
 
-#count all GBIF observation records
-occ_count(basisOfRecord='OBSERVATION')
 
-#count all GBIF records with lat long 
-occ_count(georeferenced=TRUE)
-
-occ_count(country=mexico_code, georeferenced=T, type="year")
-
-#get data for each hb species
+#get data for each hb species in winter range (don't include usa), and plot on map of central america
 for (s in unique(species)){
   key <- name_backbone(name=s, kingdom='animal')$speciesKey
-  dat <- occ_search(taxonKey=key, return='data', limit=3000, country=mexico_code, hasCoordinate=TRUE, decimalLatitude='33,10',
+  locs = data.frame('name'='name', 'key'=1, 'decimalLatitude'=1.0, 'decimalLongitude'=1.0, 'elevation'=1.0)
+  
+  for (country in unique(centralamerica)){
+  dat <- occ_search(taxonKey=key, return='data', limit=3000, country=country, hasCoordinate=TRUE,
                   fields=c('name', 'key', 'decimalLatitude', 'decimalLongitude', 'elevation'))
-  dat$name <- s # so that we just get one symbol type
-  dat = dat[dat$decimalLatitude < 35, ] #so it only encompasses the main winter range of species
-  gbifmap(input=dat, mapdatabase="world", 
-          region=c("Mexico","Honduras","Guatemala","El Salvador","Panama","Belize","Costa Rica","Nicaragua"))
+  
+  if(head(dat) != "no data found, try a different search"){ #data doesn't exist for all countries
+    if(ncol(dat) < 5){
+      dat$elevation = NA
+    }
+  
+    locs = rbind(locs, dat)
+    }}
+  
+  locs=locs[-1,] #delete the first row of dummy data
+  locs$name <- s # so that we just get one symbol type
+  locs = locs[locs$decimalLatitude < 35 & locs$decimalLatitude > 7, ] #so it only encompasses the main winter range of species - central america
+  locs = locs[locs$decimalLongitude < -78 & locs$decimalLongitude > -120, ]
+  
+  gbifmap(input=locs, mapdatabase="world", region=centralamerica_names)
 }
