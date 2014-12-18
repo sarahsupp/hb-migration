ecc928d11df00646af8b4b49500b1f59213826b8
Tina Cormier <tcormier@whrc.org>
2014-12-18 16:47:07 -0500

    working to update sampling of eBird presence and absence data. Wrote new/adapted Sarah's existing code to subset presence data by time as it relates to migration times defined in Supp et al ecopshere paper. Wrote functions and lines for spatial subsetting by flyway.

--- hb-prep_subset_abs_eBird.R
+++ hb-prep_subset_abs_eBird.R
@@ -1,11 +1,12 @@
 # Subset absent data based on number of present points - for data reduction!
 # 1. Clip by flyway
 
-library(sp)
+# Libraries needed for this code
 library(maptools)
-library(rgeos)
 library(raster)
 library(rgdal)
+library(alphahull)
+library(lubridate)
 #
 source("/Users/tcormier/Documents/scripts/git_repos/hb-migration/migration-fxns.r")
 source("/Users/tcormier/Documents/scripts/git_repos/hb-migration/hb_RS_functions.R")
@@ -14,76 +15,164 @@ source("/Users/tcormier/Documents/scripts/git_repos/hb-migration/hb_RS_functions
 # for reproducibility
 set.seed(55)
 
-wd = "/Users/tcormier/Documents/820_Hummingbirds/migration_study/data/ebird/"
+############### Inputs #####################
+#wd = "/Users/tcormier/Documents/820_Hummingbirds/migration_study/data/ebird/"
 #wd = "/mnt/d/temp/tcormier/820_hummingbirds/migration_study/data/ebird/"
 setwd(wd)
 
-# study area boundary (should be the western flyway, except for Ruby): **THIS SHOULD BE JUST THE DIRECTORY!
+#Directory containing eBird observation files
+data.dir <- "/Users/tcormier/Documents/820_Hummingbirds/migration_study/data/ebird/"
+
+# Directory containing eastern and western flyway study area boundaries (all spp western flyway, except for Ruby, and Rufous is both): 
 # SCRIPT LOGIC FOR WHICH FLYWAY TO USE DEPENDING ON SPP.
-sa.file <- "/Users/tcormier/Documents/820_Hummingbirds/migration_study/boundaries/western_flyway_dissolve_envelope.shp"
+sa.dir <- "/Users/tcormier/Documents/820_Hummingbirds/migration_study/boundaries/"
 #sa.file <- "/mnt/d/temp/tcormier/820_hummingbirds/migration_study/boundaries/western_flyway_dissolve_envelope.shp"
 
 #spp (this is how we look for input and name output files)
-#spp.list <- c("bchu", "bthu","cahu","rthu","ruhu")
+#spp.list <- c("bchu", "bthu","cahu","rthu","ruhu_east", "ruhu_west")
 #spp.list <- c("ruhu")
-spp <- "bchu"
-# List present and absent files
-abs_files = list.files(path = sprintf("%sabsent_points/migrants/original/",wd), pattern = "*\\.txt", recursive=FALSE, full.names=TRUE)
-pres_files = list.files(path = sprintf("%spresent_points/",wd), pattern = "*\\.txt", recursive=FALSE, full.names=TRUE)
 
 #directory containing files with spring and fall migration dates (from Ecosphere paper)
 mig.dir <- "/Users/tcormier/Documents/820_Hummingbirds/migration_study/data/supp_migration/" 
 
-############# One-TIME operation on absent points - then can comment out ############
-#Sarah already grouped the present points by Group ID. Need to do the same for absent points
-#sent by FAL (ONE-TIME) 
+############################################
+# Sarah already grouped the present points by Group ID. Need to do the same for absent points
+# sent by FAL (ONE-TIME) 
 
-# study area boundary
-sa <- readShapePoly(sa.file)
+# List present and absent files
+abs_files = list.files(path = sprintf("%sabsent_points/migrants/original/",data.dir), pattern = "*\\.txt", recursive=FALSE, full.names=TRUE)
+pres_files = list.files(path = sprintf("%spresent_points/",data.dir), pattern = "*\\.txt", recursive=FALSE, full.names=TRUE)
 
-# Assign projection
-proj4string(sa) <- CRS("+proj=longlat +datum=WGS84")
+spp <- "ruhu_west"
+# For each species, lets bring in the eBird data, and clip it to the migration season. For this task, migration will be defined 
+# within each year, using the dates from the analysis in Supp et al. 2015 (Ecosphere). We will begin one week prior to the onset 
+# of spring migration date. Counting forward, we will end one week after the week that contains the end of fall migration. The data 
+# will be returned to a new folder called "ebird_weeks" in the data directory, where it can be used later.
+# loop over spp, putting ruhu in twice so we can split by eastern flyway and western. 
 
-spp <- "ruhu"
-
-# loop over spp (note that for ruhu, need some logic to split eBird obs by western flyway,
-# then by eastern flyway).
-#for (spp in spp.list) {
+for (spp in spp.list) {
   print(paste0("working on ", spp))
+  # Open present and absent data files for spp and make them spatial (important later for spatial subsetting).
   pfile <- grep(pattern=spp,x=pres_files,value=T)
   afile <- grep(pattern=spp,x=abs_files,value=T)
   pdata = read.table(pfile, header=TRUE, sep=",", quote='"', fill=TRUE, as.is=TRUE, comment.char="")
   adata = read.table(afile, header=TRUE, sep="\t", quote="", fill=TRUE, as.is=TRUE, comment.char="")
   
-  # Make eBird data spatial.
+  # Some structuring of the tables
+  pdata$MONTH = factor(pdata$MONTH, levels=c(1:12), ordered=TRUE)
+  
+  # format date in absence data 
+  d <- paste(adata$DAY, adata$YEAR, sep="_")
+  adata$DATE <- as.Date(d, "%j_%Y")
+  adata$MONTH <- month(adata$DATE)
+  
+  # Now subset by dates - starting with only years >2007
+  pdata.subyr <- pdata[pdata$YEAR > 2007,]
+  adata.subyr <- adata[adata$YEAR > 2007,]
+  
+  #Read in migration dates
+  mig.files <- list.files(mig.dir, pattern="*.txt", full.names = T)
+  mig.spp <- grep(pattern=spp,x=mig.files,value=T)
+  mig.dates <- read.table(mig.spp, header=T, sep=" ")
+  names(mig.dates) <- c("spr_begin", "peak_lat", "fal_begin", "fal_end", "species", "year")
+  
+  years = unique(pdata.subyr$YEAR)
+  
+  # trim data from the beginning of the year up to 1 week before the onset of spring migration and at least 1 
+  # full week past the end of autumn migration. Start is defined as one week before the onset of spring migration;
+  # End is defined as one week past the week that contains the end of autumn migration.
+  # New columns will be added for increment (number of days since "start") and week (weeks since "start")
+  sf <- as.data.frame(matrix(data=NA, nrow=0, ncol=ncol(pdata.subyr)+2))
+  names(sf) <- c(names(pdata.subyr), "increment", "week")
+  
+  # This is from Sarah's code. *Ended here 12/18 - does it make sense to do the same for the absence data
+  # at this point as well? Think about it!
+  for (yr in years){
+    dat.yr <- pdata.subyr[pdata.subyr$YEAR == yr,]
+    start <-  mig.dates$spr_begin[mig.dates$year == yr] - 7
+    n.weeks <- floor(((mig.dates$fal_end[mig.dates$year == yr] - start)/7) + 2)
+    end <- start + n.weeks * 7
+    dat.sf <- dat.yr[dat.yr$DAY >= start & dat.yr$DAY <= end,]
+    dat.sf$increment <- dat.sf$DAY - start + 1
+    dat.sf$week <- ceiling(dat.sf$increment/7)
+    
+    sf=rbind(sf, dat.sf)
+  }
+  sf2 = sf[,c(1,2,13,14,3,4,18,5,17,19)] #take out all the extra columns that we don't really need here
+  
+  
+  # Now for spatial subsetting by flyway. Make eBird data spatial. ***Fix variable names here because moved this down from earlier in the script - want to use the date-subsetted versions ***
   coordinates(pdata) <- ~LONGITUDE+LATITUDE
   coordinates(adata) <- ~LONGITUDE+LATITUDE
-
+  
   # Assign projection
   proj4string(pdata) <- CRS("+proj=longlat +datum=WGS84")
   proj4string(adata) <- CRS("+proj=longlat +datum=WGS84")
+  
+  # flyway boundaries for the spp - will open both of them, since they are small. Otherwise, might consider adding 
+  # logic for which one to open based on spp.
+  sa.files <- c(paste0(sa.dir, "western_flyway_dissolve_envelope.shp"),paste0(sa.dir, "eastern_flyway_dissolve_envelope.shp"))
+  sa.west <- readShapePoly (sa.files[1])
+  proj4string(sa.west) <- CRS("+proj=longlat +datum=WGS84")
+  sa.east <- readShapePoly (sa.files[2])
+  proj4string(sa.east) <- CRS("+proj=longlat +datum=WGS84")
+  
+  #Can delete after you're sure you don't need this logic!
+#   if (spp != "ruhu" & spp != "rthu") {
+#     sa.file <- paste0(sa.dir, "western_flyway_dissolve_envelope.shp")
+#     # study area boundary
+#     sa <- readShapePoly(sa.file)
+#     # Assign projection
+#     proj4string(sa) <- CRS("+proj=longlat +datum=WGS84")
+#   } else if (spp == "rthu") {
+#       sa.file <- paste0(sa.dir, "eastern_flyway_dissolve_envelope.shp")
+#       # study area boundary
+#       sa <- readShapePoly(sa.file)
+#   } else if (spp == "ruhu") {
+#       # Need to clip by both flyways
+#       sa.files <- c(paste0(sa.dir, "western_flyway_dissolve_envelope.shp"),sa.file <- paste0(sa.dir, "eastern_flyway_dissolve_envelope.shp"))
+#       
+#   }# end flyway if
+  
+
 
   # Clip observations to flyway polygons - **ADD IN LOGIC FOR RUHU (NEED TO DO ONCE BY
   # WESTERN FLYWAY, THEN AGAIN BY EASTERN). 
-  pdata.clip <- over(pdata, sa)
-  ptid <- na.omit(pdata.clip) 
-  pt.poly <- pdata[as.numeric(as.character(row.names(ptid))),]  
+  out_pdata <- paste0(dirname(pfile), "/",unlist(strsplit(basename(pfile), "\\."))[1], "_clipflyway.csv")
 
-  #write out clipped present points as shapefiles
-  out_pdata <- paste0(dirname(pfile), "/",unlist(strsplit(basename(pfile), "\\."))[1], "_clipflyway.shp")
-  #write.csv(pdata.clip, file=out_pdata, quote=F, row.names=F)
-  shapefile(pt.poly, filename=out_pdata)
-  #rm(pdata)
+  if (spp != "ruhu_east" & spp != "rthu") {
+    ptsSA <- as.data.frame(PointsPolyIntersect(pdata, sa.west)
+    #write out clipped present points as shapefiles
+    write.csv(pdata.clip, file=out_pdata, quote=F, row.names=F)
+    #clean up
+    rm(pdata)
+    # formatting the adata date in various ways
+    adata <- formatAbsent(adata)
+    # Try clipping to SA
+    adata.clip <- PointsPolyIntersect(adata, sa.west)
+    rm(adata)
+  } else if (spp == "ruhu_east" | spp == "rthu") {
+      ptsSA <- as.data.frame(PointsPolyIntersect(pdata, sa.east)
+      #write out clipped present points as shapefiles
+      write.csv(pdata.clip, file=out_pdata, quote=F, row.names=F)
+      #clean up
+      rm(pdata)
+      # formatting the adata date in various ways
+      adata <- formatAbsent(adata)
+      # Try clipping to SA
+      adata.clip <- PointsPolyIntersect(adata, sa.east)
+      rm(adata)
+  } else {
+    print(paste0("accepted spp include bchu", "bthu","cahu","rthu","ruhu_east", "ruhu_west""))
+  }
   
-  #formatting the adata date in various ways
-  adata$DAY <- format(adata$DAY, format="%j")
-  adata$YEAR <- format(adata$YEAR, format="%Y")
-  dt <- paste(adata$DAY, adata$YEAR, sep="_")
-  d2 <- as.Date(dt, "%j_%Y")
-  adata$DATE <- d2
-  adata$MONTH <- format(adata$DATE, "%m")
-  adata$DOM <- format(adata$DATE, "%d")
   
+  
+  
+  
+  
+  
+  ## OLD SAMPLING ##
   #subset by date, then take random sample, as there are too many records to work with! 
   #Need to figure out appropriate number of samples (accounting for years, spatial stuff, etc.), 
   #but for now...
