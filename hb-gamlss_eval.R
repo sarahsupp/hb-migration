f67d2d9e9ba57deec55bfd4695b8b19a5be6c1ef
Sarah Supp <sarah@weecology.org>
2015-08-03 14:44:54 -0500

    finishes function to output model results and save top models to output folder for each season, raw and meaned data

--- hb-gamlss_eval.R
+++ hb-gamlss_eval.R
@@ -2,6 +2,7 @@
 # (c) 2015 by Sarah R. Supp, Laura Graham, and Frank La Sorte
 
 # import libraries
+library(ggplot2)
 library(nlme)
 library(lme4)
 library(lubridate)
@@ -29,7 +30,7 @@ species <- c("bchu", "bthu", "cahu", "ruhu", "rthu")
 #             FUNCTION TO EVALUATE GAMLSS MODELS
 ###########################################################################################
 
-eval_gammlss_models = function(dat=dat, sp=sp, lag=FALSE, means=FALSE) {
+eval_gamlss_models = function(dat=dat, sp=sp, season="season", lag=FALSE, means=FALSE, output.dir=stat.dir) {
   # evaluates gamlss models for each dataset based on the hypothesis testing framework
   # models are run on centered and scaled (such that mean=0, sd=1) predictor variables
   # models input year and window (alpha hull) as random effects
@@ -38,9 +39,17 @@ eval_gammlss_models = function(dat=dat, sp=sp, lag=FALSE, means=FALSE) {
   # if means==FALSE, the data represents all the records from the alpha hulls
   # if means==TRUE, the data has been averaged by window (each window represents a unique alpha hull)
   # outputs model results, AIC comparison, R2, and plots the data
-
+  # top model(s) are written to output.dir
+  
+  #convert year to numeric for the models
+  dat$year = as.numeric(dat$year)
+  
   if(!means){
     cat("data represents all records from the alpha hulls \n")
+    
+    #only include columns for the means (NAs in rows with N<2 give errors in gamlss)
+    dat = dat[,names(dat) %in% c("pres", "year", "window", "location.lat", "location.long", "EVI", "t10m", "SRTM_elev")]
+    dat = na.omit(dat) #remove any lingering rows with NA
     
     #-----For within-region signal of presence (presence vs. absence points)
     if(!lag){
@@ -49,28 +58,29 @@ eval_gammlss_models = function(dat=dat, sp=sp, lag=FALSE, means=FALSE) {
       # H0a. Remotely sensed variables will not be predictive of hummingbird presence, 
       #     because they do not adequately capture the processes (resources, demand) 
       #     that determine where birds are found along their migration route. 
-      MNull = gamlss(pres ~ 1 + (1|yr) + (1|window), data=dat)
+      MNull = gamlss(pres ~ 1 + (1|year) + (1|window), family="BI", data=dat)
       
       # H1. In the spring, birds should be more strongly tied to EVI, assuming that greenness 
       #     is a better proxy for food resources in the spring than in the fall.
       # H2. In the fall birds should be constrained to temperatures and Elevation*EVI interaction (resources), 
       #     particularly in the west where there is more extreme topography and habitat heterogeneity.
-      M1 = gamlss(pres ~ scale(EVI) * scale(t10m) * scale(STRM_elev) + (1|yr) + (1|window),data=dat)
-      M2 = gamlss(pres ~ scale(EVI) + scale(t10m) + scale(SRTM_elev) + (1|yr) + (1|window), data=dat)
-      M3 = gamlss(pres ~ scale(EVI) + scale(t10m) + (1|yr) + (1|window), data=dat)
-      M4 = gamlss(pres ~ scale(EVI) + (1|yr) + (1|window), data=dat)
-      M5 = gamlss(pres ~ scale(t10m) + (1|yr) + (1|window), data=dat)
+      M1 = gamlss(pres ~ scale(EVI) * scale(t10m) * scale(SRTM_elev) + (1|year) + (1|window), family="BI", data=dat)
+      M2 = gamlss(pres ~ scale(EVI) + scale(t10m) + scale(SRTM_elev) + (1|year) + (1|window), family="BI", data=dat)
+      M3 = gamlss(pres ~ scale(EVI) + scale(t10m) + (1|year) + (1|window), family="BI", data=dat)
+      M4 = gamlss(pres ~ scale(EVI) + (1|year) + (1|window), family="BI", data=dat)
+      M5 = gamlss(pres ~ scale(t10m) + (1|year) + (1|window), family="BI", data=dat)
       
-      GAIC(MNull, M1, M2, M3, M4, M5) #returns matrix with AIC and df 
+      #AIC comparison of models, returns matrix with AIC and df, ordered with best model first 
+      AIC.df = GAIC(MNull, M1, M2, M3, M4, M5) 
+      # Calculate AIC weights (Hobbs and Hilborn 2006)
+      AIC.df$w = round(Akaike.weight(AIC.df$AIC),2)
       
-      # return Cox Snell R2, R-squared = 1-(L(0)/L(fitted))^(2/n)
-      # from: http://rpackages.ianhowson.com/cran/gamlss/man/Rsq.html
-      MNull.rsq= Rsq(MNull, type="Cox Snell")
-      M1.rsq = Rsq(M1, type="Cox Snell")
-      M2.rsq = Rsq(M2, type="Cox Snell")
-      M3.rsq = Rsq(M3, type="Cox Snell")
-      M4.rsq = Rsq(M4, type="Cox Snell")
-      M5.rsq = Rsq(M5, type="Cox Snell")
+      # save model summaries for those with weight > 0
+      gamlss.list = list()
+      sig.models = row.names(AIC.df[AIC.df$w>0,])
+      for (mod in 1:length(sig.models)) { gamlss.list[[sig.models[mod]]] = get(sig.models[mod]) }
+      save(gamlss.list, file=paste0(output.dir, "/", sp, ".", season, ".gamlss.list.rda"))
+      rm(gamlss.list)
     }
     
     #-----For migratory trajectory (presence vs +/- 15 day points)
@@ -80,7 +90,7 @@ eval_gammlss_models = function(dat=dat, sp=sp, lag=FALSE, means=FALSE) {
       # H0b. Remotely sensed variables will not be predictive of hummingbird presence, 
       #     because they do not adequately capture the processes (resources, demand) 
       #     that determine where birds are found along their migration route.
-      MNull = gammlss(pres ~ 1 + (1|yr) + (1|window), data=pmin.spring)
+      MNull = gamlss(pres ~ 1 + (1|year) + (1|window), family="BI", data=dat)
       
       # H3. In the spring, birds will be more strongly tied to EVI “greening up” 
       #     as they move northward to breeding grounds than in the fall.
@@ -89,51 +99,58 @@ eval_gammlss_models = function(dat=dat, sp=sp, lag=FALSE, means=FALSE) {
       # H5. In the fall, birds should be associated with higher elevations than in the spring, 
       #     because flower phenology (blooming time) is later at higher altitudes 
       #     (so it is associated with more resources) and cooler temperatures are found at higher elevations.
-      M1 = gammlss(pres ~ scale(EVI) * scale(t10m) * scale(SRTM_elev) + (1|yr) + (1|window), data=dat)
-      M2 = gammlss(pres ~ scale(temp) * scale(elev) + scale(EVI) * scale(SRTM_elev) + (1|yr) + (1|window), data=dat)
-      M3 = gammlss(pres ~ scale(EVI) + scale(t10m) + scale(SRTM_elev) + (1|yr) + (1|window), data=dat)
-      M4 = gammlss(pres ~ scale(EVI) + scale(t10m) + (1|yr) + (1|window), data=dat)
-      M5 = gammlss(pres ~ scale(EVI) + (1|yr) + (1|window), data=dat)
-      M6 = gammlss(pres ~ scale(t10m) + (1|yr) + (1|window), data=dat)
+      M1 = gamlss(pres ~ scale(EVI) * scale(t10m) * scale(SRTM_elev) + (1|year) + (1|window), family="BI", data=dat)
+      M2 = gamlss(pres ~ scale(t10m) * scale(SRTM_elev) + scale(EVI) * scale(SRTM_elev) + (1|year) + (1|window), family="BI", data=dat)
+      M3 = gamlss(pres ~ scale(EVI) + scale(t10m) + scale(SRTM_elev) + (1|year) + (1|window), family="BI", data=dat)
+      M4 = gamlss(pres ~ scale(EVI) + scale(t10m) + (1|year) + (1|window), family="BI", data=dat)
+      M5 = gamlss(pres ~ scale(EVI) + (1|year) + (1|window), family="BI", data=dat)
+      M6 = gamlss(pres ~ scale(t10m) + (1|year) + (1|window), family="BI", data=dat)
       
+      #AIC comparison of models, returns matrix with AIC and df, ordered with best model first 
+      AIC.df = GAIC(MNull, M1, M2, M3, M4, M5, M6) 
+      # Calculate AIC weights (Hobbs and Hilborn 2006)
+      AIC.df$w = round(Akaike.weight(AIC.df$AIC),2)
       
-      GAIC(MNull, M1, M2, M3, M4, M5, M6) #returns matrix with AIC and df 
-      
-      # return Cox Snell R2, R-squared = 1-(L(0)/L(fitted))^(2/n);  http://rpackages.ianhowson.com/cran/gamlss/man/Rsq.html
-      MNull.rsq= Rsq(MNull, type="Cox Snell")
-      M1.rsq = Rsq(M1, type="Cox Snell")
-      M2.rsq = Rsq(M2, type="Cox Snell")
-      M3.rsq = Rsq(M3, type="Cox Snell")
-      M4.rsq = Rsq(M4, type="Cox Snell")
-      M5.rsq = Rsq(M5, type="Cox Snell")
-      M6.rsq = Rsq(M6, type="Cox Snell")
+      # save model summaries for those with weight > 0
+      gamlss.list = list()
+      sig.models = row.names(AIC.df[AIC.df$w>0,])
+      for (mod in 1:length(sig.models)) { gamlss.list[[sig.models[mod]]] = get(sig.models[mod]) }
+      save(gamlss.list, file=paste0(output.dir, "/", sp, ".", season, ".gamlss.list.rda"))
+      rm(gamlss.list)
     }
   }
+  
   if(means){
     cat("data has been averaged within unique alpha hulls (window) \n")
     cat("effort will be accounted for as number of checklists where species was present \n")
+    
+    #only include columns for the means (NAs in rows with N<2 give errors in gamlss)
+    dat = dat[,names(dat) %in% c("pres", "yday1", "year", "window", "N", "mean.lat", "mean.lon", "mean.EVI", "mean.t10m", "mean.elev")]
+    dat = na.omit(dat) #remove any lingering rows with NA
     
     #-----For within-region signal of presence (presence vs. absence points)
     if(!lag){
       cat("models will compare within region presence vs. absence \n")
       
       #models use the same basic hypotheses as above, for no lag, but include effort
-      MNull = gamlss(pres ~ 1 + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M1 = gamlss(pres ~ scale(mean.EVI) * scale(mean.t10m) * scale(mean.elev) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M2 = gamlss(pres ~ scale(mean.EVI) + scale(mean.t10m) + scale(mean.elev) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M3 = gamlss(pres ~ scale(mean.EVI) + scale(mean.t10m) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M4 = gamlss(pres ~ scale(mean.EVI) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M5 = gamlss(pres ~ scale(mean.t10m) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
+      MNull = gamlss(pres ~ 1 + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M1 = gamlss(pres ~ scale(mean.EVI) * scale(mean.t10m) * scale(mean.elev) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M2 = gamlss(pres ~ scale(mean.EVI) + scale(mean.t10m) + scale(mean.elev) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M3 = gamlss(pres ~ scale(mean.EVI) + scale(mean.t10m) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M4 = gamlss(pres ~ scale(mean.EVI) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M5 = gamlss(pres ~ scale(mean.t10m) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
       
-      GAIC(MNull, M1, M2, M3, M4, M5, M6) #returns matrix with AIC and df 
+      #AIC comparison of models, returns matrix with AIC and df, ordered with best model first 
+      AIC.df = GAIC(MNull, M1, M2, M3, M4, M5) 
+      # Calculate AIC weights (Hobbs and Hilborn 2006)
+      AIC.df$w = round(Akaike.weight(AIC.df$AIC),2)
       
-      # return Cox Snell R2, R-squared = 1-(L(0)/L(fitted))^(2/n);  http://rpackages.ianhowson.com/cran/gamlss/man/Rsq.html
-      MNull.rsq= Rsq(MNull, type="Cox Snell")
-      M1.rsq = Rsq(M1, type="Cox Snell")
-      M2.rsq = Rsq(M2, type="Cox Snell")
-      M3.rsq = Rsq(M3, type="Cox Snell")
-      M4.rsq = Rsq(M4, type="Cox Snell")
-      M5.rsq = Rsq(M5, type="Cox Snell")
+      # save model summaries for those with weight > 0
+      gamlss.list = list()
+      sig.models = row.names(AIC.df[AIC.df$w>0,])
+      for (mod in 1:length(sig.models)) { gamlss.list[[sig.models[mod]]] = get(sig.models[mod]) }
+      save(gamlss.list, file=paste0(output.dir, "/", sp, ".", season, ".gamlss.means.list.rda"))
+      rm(gamlss.list)
     }
     
     #-----For migratory trajectory (presence vs +/- 15 day points)
@@ -141,32 +158,38 @@ eval_gammlss_models = function(dat=dat, sp=sp, lag=FALSE, means=FALSE) {
       cat("models will compare presence with migratory trajectory lags (+/- 15 days \n")
       
       #models use the same basic hypotheses as above, for +/- 15 day lag, but include effort
-      MNull = gammlss(pres ~ 1 + (1|yr), sigma.formula=pb(N), data=pmin.spring)
-      M1 = gammlss(pres ~ scale(mean.EVI) * scale(mean.t10m) * scale(mean.elev) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M2 = gammlss(pres ~ scale(mean.t10m) * scale(mean.elev) + scale(mean.EVI) * scale(mean.elev) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M3 = gammlss(pres ~ scale(mean.EVI) + scale(mean.t10m) + scale(mean.elev) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M4 = gammlss(pres ~ scale(mean.EVI) + scale(mean.t10m) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M5 = gammlss(pres ~ scale(mean.EVI) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
-      M6 = gammlss(pres ~ scale(mean.t10m) + (1|yr) + (1|window), sigma.formula=pb(N), data=dat)
+      MNull = gamlss(pres ~ 1 + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M1 = gamlss(pres ~ scale(mean.EVI) * scale(mean.t10m) * scale(mean.elev) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M2 = gamlss(pres ~ scale(mean.t10m) * scale(mean.elev) + scale(mean.EVI) * scale(mean.elev) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M3 = gamlss(pres ~ scale(mean.EVI) + scale(mean.t10m) + scale(mean.elev) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M4 = gamlss(pres ~ scale(mean.EVI) + scale(mean.t10m) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M5 = gamlss(pres ~ scale(mean.EVI) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
+      M6 = gamlss(pres ~ scale(mean.t10m) + (1|year) + (1|window), sigma.formula=pb(N), family="BI", data=dat)
       
-      GAIC(MNull, M1, M2, M3, M4, M5, M6) #returns matrix with AIC and df 
+      #AIC comparison of models, returns matrix with AIC and df, ordered with best model first 
+      AIC.df = GAIC(MNull, M1, M2, M3, M4, M5, M6) 
+      # Calculate AIC weights (Hobbs and Hilborn 2006)
+      AIC.df$w = round(Akaike.weight(AIC.df$AIC),2)
       
-      # return Cox Snell R2, R-squared = 1-(L(0)/L(fitted))^(2/n);  http://rpackages.ianhowson.com/cran/gamlss/man/Rsq.html
-      MNull.rsq= Rsq(MNull, type="Cox Snell")
-      M1.rsq = Rsq(M1, type="Cox Snell")
-      M2.rsq = Rsq(M2, type="Cox Snell")
-      M3.rsq = Rsq(M3, type="Cox Snell")
-      M4.rsq = Rsq(M4, type="Cox Snell")
-      M5.rsq = Rsq(M5, type="Cox Snell")
-      M6.rsq = Rsq(M6, type="Cox Snell")
-    } 
+      # save model summaries for those with weight > 0
+      gamlss.list = list()
+      sig.models = row.names(AIC.df[AIC.df$w>0,])
+      for (mod in 1:length(sig.models)) { gamlss.list[[sig.models[mod]]] = get(sig.models[mod]) }
+      save(gamlss.list, file=paste0(output.dir, "/", sp, ".", season, ".gamlss.means.list.rda"))
+      rm(gamlss.list)
+    }
   }
-  
   #Whatever set of models was run, find a way to write out the model eval step and the R2 values into a big table
   #save the modesl to the stats folder
   #print the table to the screen, and save to a txt file to see later.
-  mod.eval = cbind(R2, GAIC)
-  return(mod.eval)
+
+  # return Cox Snell R2, R-squared = 1-(L(0)/L(fitted))^(2/n);  http://rpackages.ianhowson.com/cran/gamlss/man/Rsq.html
+  AIC.df$Rsq = rep(NA,nrow(AIC.df))
+  for(r in 1:length(row.names(AIC.df))) { r2=round(Rsq(get(row.names(AIC.df)[r]), type="Cox Snell"),4); AIC.df$Rsq[r]=r2 }
+  AIC.df$nobs = rep(NA,nrow(AIC.df))
+  for(r in 1:length(row.names(AIC.df))) { nobs=get(row.names(AIC.df)[r])$N; AIC.df$nobs[r]=nobs }
+  
+  return(AIC.df)
 }
 
 
@@ -219,34 +242,34 @@ for (sp in species){
   ppls.spring.mean = RS_means(ppls.spring)
   ppls.fall.mean = RS_means(ppls.fall)
   
+  ############################################################################
   #-----------------------Run hypothesis models and compare for best fit
+  ############################################################################
   gamlss.list <- list()
   print(sp)
   
   #----- all data points
-  eval_gamlss_models(pa.spring, sp=sp, lag=FALSE)
-  eval_gamlss_models(pa.fall, sp=sp, lag=FALSE)
+  eval_gamlss_models(pa.spring, sp=sp, season="spring", lag=FALSE, means=FALSE, output.dir=stat.dir)
+  eval_gamlss_models(pa.fall, sp=sp, season="fall", lag=FALSE, means=FALSE, output.dir=stat.dir)
     
-  eval_gamlss_models(pmin.spring, sp=sp, lag=TRUE)
-  eval_gamlss_models(pmin.fall, sp=sp, lag=TRUE)
+  eval_gamlss_models(pmin.spring, sp=sp, season="springmin", lag=TRUE, means=FALSE, output.dir=stat.dir)
+  eval_gamlss_models(pmin.fall, sp=sp, season="fallmin", lag=TRUE, means=FALSE, output.dir=stat.dir)
   
-  eval_gamlss_models(ppls.spring, sp=sp, lag=TRUE)
-  eval_gamlss_models(ppls.fall, sp=sp, lag=TRUE)
+  eval_gamlss_models(ppls.spring, sp=sp, season="springpls", lag=TRUE, means=FALSE, output.dir=stat.dir)
+  eval_gamlss_models(ppls.fall, sp=sp, lag=TRUE, season="fallpls", means=FALSE, output.dir=stat.dir)
   
   #----- means
-  eval_gamlss_models(pa.spring.mean, sp=sp, lag=FALSE, means=TRUE)
-  eval_gamlss_models(pa.fall.mean, sp=sp, lag=FALSE, means=TRUE)
+  eval_gamlss_models(pa.spring.mean, season="spring", sp=sp, lag=FALSE, means=TRUE, output.dir=stat.dir)
+  eval_gamlss_models(pa.fall.mean, sp=sp, season="fall", lag=FALSE, means=TRUE, output.dir=stat.dir)
   
-  eval_gamlss_models(pmin.spring.mean, sp=sp, lag=FALSE, means=TRUE)
-  eval_gamlss_models(pmin.fall.mean, sp=sp, lag=FALSE, means=TRUE)
+  eval_gamlss_models(pmin.spring.mean, season="springmin", sp=sp, lag=TRUE, means=TRUE, output.dir=stat.dir)
+  eval_gamlss_models(pmin.fall.mean, sp=sp, season="fallmin", lag=TRUE, means=TRUE, output.dir=stat.dir)
   
-  eval_gamlss_models(ppls.spring.mean, sp=sp, lag=FALSE, means=TRUE)
-  eval_gamlss_models(ppls.fall.mean, sp=sp, lag=FALSE, means=TRUE)
+  eval_gamlss_models(ppls.spring.mean, season="springpls", sp=sp, lag=TRUE, means=TRUE, output.dir=stat.dir)
+  eval_gamlss_models(ppls.fall.mean, sp=sp, season="fallpls", lag=TRUE, means=TRUE, output.dir=stat.dir)
   
-  
-  #Identify best model using AICc. Assess pvalues and R2 for best model(s).
-  #Look at tables in mod.eval output. 
-  #Look at list of model output saved as gamlss.list files in the stats folder.
+}
+
   
   
   
